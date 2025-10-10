b605cb17f8c39d39a2453e70553f653528e1a92f
Umang Saxena <hanusaxena68@gmail.com>
2025-10-10 19:42:58 +0530

    UI bigs Fixed

--- components/RateRangeSlider.tsx
+++ components/RateRangeSlider.tsx
@@ -30,5 +30,10 @@ const RateRangeSlider = ({ minRate, maxRate, onMinRateChange, onMaxRateChange }:
           value={maxRate}
           onChange={(e) => onMaxRateChange(Number(e.target.value))}
           className="w-full"
+        />
+      </div>
+    </div>
+  );
+};
 
 export default RateRangeSlider;
